cbc8de980f6d9794fee8e6fffdea6a7759e3af1b
ismailguzel <ismailgzel@gmail.com>
2025-10-22 15:45:00 +0300

    Clean main branch for new modular library

--- time-series-generator/timeseries_generator/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-"""
-TimeSeriesGenerator Package
-
-A comprehensive toolkit for generating synthetic time series data with various characteristics and structural breaks for testing and educational purposes.
-"""
-
-from .generator import TimeSeriesGenerator
-
-__version__ = '0.1.0'
